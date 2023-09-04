dc7743f49c8b7ed94e189e13795d77595c543cb9
Kgodisho <kgodisholebopa@gmail.com>
2023-09-04 11:27:20 +0200

    about page main elements

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,6 +1,40 @@
 <template>
-  <div>
-    <h1>This is a page</h1>
+  <div class="container-fluid">
+    <!-- Add a video of apparel vibes -->
+    <video src=""></video>
+    <div class="container text-center">
+      <h1>
+        Our Story:
+      </h1>
+      <p>
+        At Anchored Life, we believe that faith isn't just something you carry in your heart; it's a message you can wear proudly. Our journey began with a simple yet profound idea: to create a space where faith and fashion converge seamlessly.We understand that what you wear is an extension of who you are, and it can reflect your values, beliefs, and the things that matter most to you. This belief sparked the creation of our brand.
+        <br>
+        <br>
+        Our mission is clear: to provide you with a curated collection of Christian apparel that not only looks good but also serves as a powerful statement of your faith. Every piece we offer is carefully designed to reflect the timeless messages of hope, love, and grace found in the Scriptures. When you shop with us, you're not just purchasing clothing; you're joining a movement, a community of individuals who embrace faith and fashion as integral parts of their lives.
+        <br>
+        <br>
+        Thank you for being a part of our journey. We're excited to have you here, and we can't wait to see how your faith and style will make a difference in the world.
+        <br>
+        <br>
+        Welcome to Anchored Life.
+      </p>
+    </div>
+    <br>
+    <div class="container text-center">
+  <div class="row">
+    <h1> Read Our Latest Blog Posts Here </h1>
+    <div class="col text-center" id="blogpost" style="padding: 3%;">
+    <p> The Most Important Article <br> We Will Ever Write.</p>
+    </div>
+    <div class="col text-center" id="blog" style="padding: 3%">
+    <p>Cross T shirts and Their Designs</p>
+    </div>
+  </div>
+  </div>
+  <br>
+  <div class="container">
+Contact Info
+  </div>
   </div>
 </template>
 
@@ -8,5 +42,19 @@
 </script>
 
 <style scoped>
+p{
+  text-align: justify;
 
+}
+
+#blog, #blogpost{
+  background-color: black;
+  border: 7px white solid;
+  border-radius: 10px;
+  color: white; 
+  font-size: 1.5rem;
+  font-weight: 900;
+  padding-left: 2%;
+  text-align: center;
+}
 </style>
